4b020718f3f7341e8767b7005883e9f2ce68b315
arthur-rule <arthur.rule@cydeo.com>
2025-03-23 00:03:25 +0300

    Week1 completed

--- src/test/java/com/cydeo/liveWeekend/week1/P01_SimpleGETRequest.java
+++ src/test/java/com/cydeo/liveWeekend/week1/P01_SimpleGETRequest.java
@@ -4,9 +4,11 @@ import com.cydeo.utilities.HrTestBase;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class P01_SimpleGETRequest extends HrTestBase {
     /**
@@ -37,6 +39,7 @@ public class P01_SimpleGETRequest extends HrTestBase {
 //* - Given accept type is Json
 //* - And base URI: http://34.226.136.145:1000/ords/hr
 //* - When user sends GET request to endpoints:/regions
+//* - Store the response in Response Object that comes from GET Request
 
 Response response = given()
                         .log().uri()
@@ -44,17 +47,46 @@ Response response = given()
                     .when()
                         .get("/regions");
 
-
-//* - Store the response in Response Object that comes from GET Request
 //* - Print out followings:
         //- Headers
+        System.out.println("--------HEADERS-----------");
+        //System.out.println(response.getHeaders());
+        System.out.println(response.headers());
+
         //- Content-Type
+        System.out.println("-----------CONTENT TYPE--------");
+        System.out.println(response.getContentType());
+
         //- Status Code
+        System.out.println("---------STATUS CODE----------");
+        System.out.println(response.statusCode());
+
         //- Response
+        System.out.println("---------RESPONSE----------");
+        System.out.println("-----------response.prettyPrint()---------");
+        response.prettyPrint();
+        System.out.println("-----------response.prettyPeek()---------");
+        response.prettyPeek();
+
+
         //- Date
+        System.out.println("---------DATE----------");
+        System.out.println(response.header("Date"));
+
+
 //* - Then verify followings:
-        //- Response body has "Europe"
         //- Response headers has "Date"
+
+        boolean hasDate = response.headers().hasHeaderWithName("Date");
+        System.out.println("hasDate = " + hasDate);
+        assertTrue(hasDate);
+
+        //- Response body has "Europe"
+        boolean hasEurope = response.asString().contains("Europe");
+        System.out.println("hasEurope = " + hasEurope);
+        assertTrue(hasEurope);
+
+
 
     }
 }
