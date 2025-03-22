269f0fdc30bd080f3367bfa598167c053c2ab27a
arthur-rule <arthur.rule@cydeo.com>
2025-03-22 22:51:34 +0300

    Requirement added

---
+++ src/test/java/com/cydeo/liveWeekend/week1/P01_SimpleGETRequest.java
@@ -0,0 +1,20 @@
+package com.cydeo.liveWeekend.week1;
+
+public class P01_SimpleGETRequest {
+    /**
+     * Requirements:
+     * - Given accept type is Json
+     * - And base URI: http://34.226.136.145:1000/ords/hr
+     * - When user sends GET request to endpoints:/regions
+     * - Store the response in Response Object that comes from GET Request
+     * - Print out followings:
+         - Headers
+         - Content-Type
+         - Status Code
+         - Response
+         - Date
+     * - Then verify followings:
+         - Response body has "Europe"
+         - Response headers has "Date"
+     */
+}
--- src/test/java/com/cydeo/liveWeekend/week1/practice.java
+++
@@ -1,6 +0,0 @@
-package com.cydeo.liveWeekend.week1;
-
-public class practice {
-
-    //If you see this line then You are OK
-}
